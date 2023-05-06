59ab6c4abc4105319d18a9c43fa3952036c4fcd4
NAME <mail@gmail.com>
2023-05-06 10:08:13 +0100

    .

--- setup.sh
+++ setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-#apt update &&  apt upgrade
-#apt install php -y
-#pkg install php -y
-#apt install ssh -y
-#pkg install ssh -y
-#git config --global user.email test@gmail.com
-#git config ---global user.name test
-#git clone https://github.com/Enthernetcode/work
-#cd
+apt update &&  apt upgrade
+apt install php -y
+pkg install php -y
+apt install ssh -y
+pkg install ssh -y
+git config --global user.email test@gmail.com
+git config ---global user.name test
+git clone https://github.com/Enthernetcode/work
+cd
 echo '''
 cd work
 php -S 127.0.0.1:8000''' > fast
