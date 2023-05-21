af2a5909b54103796b040c423caea45a5db1bc14
NAME <mail@gmail.com>
2023-05-21 23:47:46 +0100

    .

--- setup.sh
+++ setup.sh
@@ -11,11 +11,18 @@ git clone https://github.com/Enthernetcode/work
 cd
 echo '''
 cd work
+bash file
 php -S 127.0.0.1:8000''' > fast
 echo '''
 ssh -R 80:localhost:8000 nokey@localhost.run''' > host
-bash fast
+cd work
+mv host.sh /data/data/com.termux/files/home
+cp -r host.sh /data/data/com.termux/files/usr/bin
+cd /data/data/com.termux/files/usr/bin
+mv host.sh host
+chmod +x host
 cd 
+bash host.sh
 sleep 40
 #rm -rf work
 echo "deleting directory work"
